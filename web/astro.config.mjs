ccc5ef5791317d6d35de7181b9c3442379213436
harutin <godaharuto@gmail.com>
2026-01-25 15:01:26 +0900

    Remove Cloudflare adapter and related dependencies from the Astro configuration and package files.

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,10 +3,7 @@
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  adapter: cloudflare(),
+	integrations: [react()],
 });
